5426fc44546f9307822313474151ab6c15390f3d
abolos <andybolos@gmail.com>
2021-04-19 11:12:42 -0600

    wip

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ import { useHistory } from "react-router-dom";
 export default function Home() {
   // 👉 STEP 5 - Build a click handler that will imperatively
   // navigate us to <website base URL>/items-list
+  const history = useHistory()
 
   const routeToShop = () => {
-
+    // navigate to localhost:1234/items-list
+    console.log(history);
   }
 
   return (
