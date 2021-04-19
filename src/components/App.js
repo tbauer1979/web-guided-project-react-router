cfe46aeebe7ce80993c660a56410c850f5196748
abolos <andybolos@gmail.com>
2021-04-19 11:25:55 -0600

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -35,6 +35,9 @@ export default function App(props) {
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
       <Switch>
+        <Route path='/items-list/:itemID'>
+          <Item />
+        </Route>
         <Route path='/items-list'>
           <ItemsList items={stock} />
         </Route>
